b5494897991cea4b653f4a0117671a6daeb483a0
音乐 <musicyy.leo@gmail.com>
2022-12-10 02:31:56 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,12 +9,12 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Cocky Leon',
+  domain: '444491.xyz',
+  author: 'Leon',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '生活 | 技术',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
